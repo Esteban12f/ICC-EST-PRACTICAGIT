fd6f5bfb0e98d54193942b5f05d63918be8f2777
Jorgemrqz <estebanmarquez2702@gmail.com>
2024-09-16 12:16:14 -0500

    Archivo 3 ha sido agregado :[

--- Nombre.java
+++ Nombre.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Nombre {
     public static void main(String[] args) {
         
 
         int ola = 1;
+        String x;
+
+        Scanner entrada = Scanner(System.in);
+
+        System.out.println("Ingresa tu nombre");
+        
         
 
         while (ola<=10){
